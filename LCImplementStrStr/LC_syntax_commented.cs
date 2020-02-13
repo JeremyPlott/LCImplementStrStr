218070bad68f55def10d9a32222663f5b5299039
jeremy_plott <plott.j@pg.com>
2020-02-13 14:17:52 -0500

    Add project files.

---
+++ LCImplementStrStr/LC_syntax_commented.cs
@@ -0,0 +1,13 @@
+﻿```
+public class Solution
+{
+    public int StrStr(string haystack, string needle)
+    {
+
+        return haystack.IndexOf(needle);
+
+        //lol what can I really say?
+        //just use IndexOf(), it's built-in and ready to be used.
+    }
+}
+```
---
+++ LCImplementStrStr/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace LCImplementStrStr
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string haystack = "hello";
+
+            string needle = "";
+
+            Console.WriteLine(StrStr(haystack, needle));
+
+            int StrStr(string haystack, string needle)
+            {
+                return haystack.IndexOf(needle);
+            }
+        }
+    }
+}
